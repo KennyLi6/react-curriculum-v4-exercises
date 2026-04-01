936704c3186c680ecb1ffc6bb35f781dbeb02004
KennyLi6 <kennyli3219@gmail.com>
2026-04-01 15:03:55 -0700

    creating introduction

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,24 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = "Kenny Li"
+  const age = 22
+  const city = "San Francisco"
+  const hobbies = [
+    "Playing games",
+    "Hanging out with friends"
+  ]
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About {name}</h1>
+      <p>
+        I am {name}, I am {age}, and I currently reside in {city}.<br />
+        I graduated from UC Santa Cruz with a B.S. in Computer Science: Game Design in June 2025.<br />
+        A few of my hobbies: <br />
+        
+      </p>
+      <ul>{hobbies.map((hobby) => (<span key={hobby}>{hobby}{<br />}</span>))}</ul>
     </div>
   );
 }
