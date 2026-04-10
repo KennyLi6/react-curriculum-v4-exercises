a6297116170c8067036db056e99cba4be5dca90f
KennyLi6 <kennyli3219@gmail.com>
2026-04-09 18:46:59 -0700

    moving all functionality into snackapp component

--- src/exercises/lesson-02/studentWork.jsx
+++ src/exercises/lesson-02/studentWork.jsx
@@ -1,17 +1,12 @@
 //Lesson-02 Building with ReactDOM and components
 //Exercise: Build a "Snack Ranking App" Component in this file
 //Import components here
-import SnackHeader from "./SnackHeader";
-import SnackList from "./SnackList";
-import SnackFooter from "./SnackFooter";
+import SnackApp from "./SnackApp";
 
 export default function StudentWork() {
   return (
     <div>
-      {/* add JSX here */}
-      <SnackHeader />
-      <SnackList />
-      <SnackFooter />
+      <SnackApp />
     </div>
   );
 }
