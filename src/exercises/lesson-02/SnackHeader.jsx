0505d25706a0eac43f0db89a4b2191226a5bea65
KennyLi6 <kennyli3219@gmail.com>
2026-04-09 18:13:50 -0700

    added snackheader component

---
+++ src/exercises/lesson-02/SnackHeader.jsx
@@ -0,0 +1,8 @@
+function SnackHeader() {
+    const header = "Snack Rankings"
+    return (
+        <h1>{header}</h1>
+    );
+}
+
+export default SnackHeader;
--- src/exercises/lesson-02/studentWork.jsx
+++ src/exercises/lesson-02/studentWork.jsx
@@ -1,11 +1,14 @@
 //Lesson-02 Building with ReactDOM and components
 //Exercise: Build a "Snack Ranking App" Component in this file
 //Import components here
+import SnackHeader from "./SnackHeader";
+
 
 export default function StudentWork() {
   return (
     <div>
       {/* add JSX here */}
+      <SnackHeader />
       <p> Student output will go here</p>
     </div>
   );
