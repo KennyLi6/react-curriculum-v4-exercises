5ad2a5a8efd92472e4366cb0dd2c999db9711faa
KennyLi6 <kennyli3219@gmail.com>
2026-05-04 17:14:55 -0700

    modify handleSubmit

--- src/exercises/lesson-05/SnackForm.jsx
+++ src/exercises/lesson-05/SnackForm.jsx
@@ -30,15 +30,19 @@ export default function SnackForm({
 
   function handleSubmit(e) {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const name = formData.get('name');
-    const rating = formData.get('rating');
+
+    if (!validateName() || !validateRating()) {
+      setTouched({ name: true, rating: true });
+      return;
+    }
 
     if (isEditing) {
       updateSnack(editingSnack.id, name, rating);
     } else {
       addSnack(name, rating);
-      e.target.reset();
+      setName("");
+      setRating("");
+      setTouched({ name: false, rating: false });
     }
   }
 
