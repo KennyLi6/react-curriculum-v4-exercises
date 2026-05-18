9ae021aea8feb73b6314445429df9498bcc00d8d
KennyLi6 <kennyli3219@gmail.com>
2026-05-18 16:46:49 -0700

    implement getPosts

--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -4,19 +4,31 @@ import { getPosts } from './api';
 import { useEffect, useState } from 'react';
 
 export default function FetchOnRender() {
-  const [postData, setPostData] = useState(null);
+  const [postData, setPostData] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const data = getPosts()
-    setPostData([{title: "test", body: "stuff"}]);
-  }, [])
+    async function fetchPosts() {
+      try {
+        const data = await getPosts();
+        setPostData(data);
+      } catch (error) {
+        setError(`Error: ${error.name} | ${error.message}`);
+      }
+    }
+    fetchPosts();
+  }, []);
 
   return (
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
       <div className="content">
+          {error && (
+            <div>
+              <p>{error}</p>
+            </div>
+          )}
           {postData && <PostList posts={postData}/>}
-          {console.log(postData)}
       </div>
     </div>
   );
